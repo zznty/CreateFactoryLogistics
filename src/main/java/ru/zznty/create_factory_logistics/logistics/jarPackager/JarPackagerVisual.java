02b4f6ecc6fc8ab7d9f1dcf06b84dda77062d7be
zznty <94796179+zznty@users.noreply.github.com>
2025-05-20 19:33:28 +0700

    fix packager visual

--- src/main/java/ru/zznty/create_factory_logistics/logistics/jarPackager/JarPackagerVisual.java
+++ src/main/java/ru/zznty/create_factory_logistics/logistics/jarPackager/JarPackagerVisual.java
@@ -42,7 +42,7 @@ public class JarPackagerVisual<T extends JarPackagerBlockEntity> extends Abstrac
             if (facing.getAxis() == Direction.Axis.Y)
                 facing = Direction.NORTH;
 
-            var lowerCorner = Vec3.atLowerCornerOf(facing.getNormal());
+            var lowerCorner = Vec3.atLowerCornerOf(Direction.DOWN.getNormal());
 
             tray.setIdentityTransform()
                     .translate(getVisualPosition())
