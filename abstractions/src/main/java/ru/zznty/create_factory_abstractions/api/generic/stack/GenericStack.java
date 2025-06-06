017f6f345cbccdcd51decc251b99292d23d7782d
zznty <94796179+zznty@users.noreply.github.com>
2025-06-06 17:13:42 +0700

    fix itemkey stored stack not clearing count on wrap

--- abstractions/src/main/java/ru/zznty/create_factory_abstractions/api/generic/stack/GenericStack.java
+++ abstractions/src/main/java/ru/zznty/create_factory_abstractions/api/generic/stack/GenericStack.java
@@ -2,6 +2,8 @@ package ru.zznty.create_factory_abstractions.api.generic.stack;
 
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import ru.zznty.create_factory_abstractions.CreateFactoryAbstractions;
 import ru.zznty.create_factory_abstractions.api.generic.GenericFilterProvider;
 import ru.zznty.create_factory_abstractions.api.generic.key.GenericKey;
 import ru.zznty.create_factory_abstractions.generic.key.item.ItemKey;
@@ -26,11 +28,14 @@ public record GenericStack(GenericKey key, int amount) {
     }
 
     public static GenericStack wrap(ItemStack stack) {
-        if (stack.isEmpty()) return EMPTY;
-        return new GenericStack(new ItemKey(stack), stack.getCount());
+        if (stack.getItem() == Items.AIR) return EMPTY;
+        return new GenericStack(new ItemKey(stack.copyWithCount(1)), stack.getCount());
     }
 
     public static GenericStack of(FactoryPanelBehaviour behaviour) {
+        if (!CreateFactoryAbstractions.EXTENSIBILITY_AVAILABLE)
+            return GenericStack.wrap(behaviour.getFilter())
+                    .withAmount(behaviour.count * (behaviour.upTo ? 1 : behaviour.getFilter().getMaxStackSize()));
         GenericFilterProvider filterProvider = (GenericFilterProvider) behaviour;
         return filterProvider.filter();
     }
