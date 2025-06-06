c1decbe69362d99fd3a9e33b9f46058b0f763922
zznty <94796179+zznty@users.noreply.github.com>
2025-06-07 02:48:15 +0700

    add back connection handler mixin for fluid gauge

--- forge/src/main/java/ru/zznty/create_factory_logistics/mixin/logistics/panel/FactoryPanelConnectionHandlerMixin.java
+++ forge/src/main/java/ru/zznty/create_factory_logistics/mixin/logistics/panel/FactoryPanelConnectionHandlerMixin.java
@@ -1,13 +1,19 @@
 package ru.zznty.create_factory_logistics.mixin.logistics.panel;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
+import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBlock;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelConnectionHandler;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import ru.zznty.create_factory_abstractions.api.generic.GenericFilterProvider;
 import ru.zznty.create_factory_abstractions.api.generic.key.GenericKeyRegistration;
@@ -15,6 +21,47 @@ import ru.zznty.create_factory_abstractions.generic.impl.GenericContentExtender;
 
 @Mixin(FactoryPanelConnectionHandler.class)
 public class FactoryPanelConnectionHandlerMixin {
+    @ModifyExpressionValue(
+            method = "checkForIssues(Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;)Ljava/lang/String;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/foundation/blockEntity/SmartBlockEntity;getBlockState()Lnet/minecraft/world/level/block/state/BlockState;",
+                    ordinal = 0
+            )
+    )
+    private static BlockState replaceFluidPanelBlockState1(BlockState original) {
+        if (original.is(AllBlocks.FACTORY_GAUGE.get())) {
+            return original;
+        }
+
+        return createFactoryLogistics$copyBlockState(original);
+    }
+
+    @ModifyExpressionValue(
+            method = "checkForIssues(Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;Lcom/simibubi/create/content/logistics/factoryBoard/FactoryPanelBehaviour;)Ljava/lang/String;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/foundation/blockEntity/SmartBlockEntity;getBlockState()Lnet/minecraft/world/level/block/state/BlockState;",
+                    ordinal = 1
+            )
+    )
+    private static BlockState replaceFluidPanelBlockState2(BlockState original) {
+        if (original.is(AllBlocks.FACTORY_GAUGE.get())) {
+            return original;
+        }
+
+        return createFactoryLogistics$copyBlockState(original);
+    }
+
+    @Unique
+    private static @NotNull BlockState createFactoryLogistics$copyBlockState(BlockState original) {
+        return AllBlocks.FACTORY_GAUGE.getDefaultState()
+                .setValue(FactoryPanelBlock.WATERLOGGED, original.getValue(FactoryPanelBlock.WATERLOGGED))
+                .setValue(FactoryPanelBlock.POWERED, original.getValue(FactoryPanelBlock.POWERED))
+                .setValue(FactoryPanelBlock.FACING, original.getValue(FactoryPanelBlock.FACING))
+                .setValue(FactoryPanelBlock.FACE, original.getValue(FactoryPanelBlock.FACE));
+    }
+
     @WrapOperation(
             method = "panelClicked",
             at = @At(
