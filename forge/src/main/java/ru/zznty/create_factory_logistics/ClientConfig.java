7df3468bd0efa5f915a336d328783666f2ed6ef6
zznty <94796179+zznty@users.noreply.github.com>
2025-06-12 21:16:30 +0700

    fix: create font rendering too high up

--- forge/src/main/java/ru/zznty/create_factory_logistics/ClientConfig.java
+++ forge/src/main/java/ru/zznty/create_factory_logistics/ClientConfig.java
@@ -10,23 +10,10 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 public class ClientConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    private static final ForgeConfigSpec.EnumValue<FontStyle> FONT_STYLE = BUILDER
-            .comment("Style of font rendered inside stock keeper ui")
-            .defineEnum("fontStyle", FontStyle.CREATE);
-
     static final ForgeConfigSpec SPEC = BUILDER.build();
-
-    public static FontStyle fontStyle;
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
         if (event.getConfig().getType() != ModConfig.Type.CLIENT) return;
-        fontStyle = FONT_STYLE.get();
-    }
-
-    public enum FontStyle {
-        SMALL,
-        LARGE,
-        CREATE
     }
 }
