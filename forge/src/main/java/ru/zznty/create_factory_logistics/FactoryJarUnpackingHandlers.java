8e128b112bb98672920994b36b1dbaea87b331c3
zznty <94796179+zznty@users.noreply.github.com>
2025-06-26 03:36:06 +0700

    fix: give exp orbs even when player is present to account for stuff like mending

--- forge/src/main/java/ru/zznty/create_factory_logistics/FactoryJarUnpackingHandlers.java
+++ forge/src/main/java/ru/zznty/create_factory_logistics/FactoryJarUnpackingHandlers.java
@@ -1,11 +1,11 @@
 package ru.zznty.create_factory_logistics;
 
 import com.simibubi.create.api.registry.SimpleRegistry;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.registries.ForgeRegistries;
 import ru.zznty.create_factory_logistics.logistics.jar.unpack.JarUnpackingHandler;
 
 import java.util.Optional;
@@ -13,17 +13,14 @@ import java.util.Optional;
 public class FactoryJarUnpackingHandlers {
     public static void register() {
 
-        Optional<Fluid> ceiExperience = BuiltInRegistries.FLUID.getOptional(
-                ResourceLocation.fromNamespaceAndPath("create_enchantment_industry", "experience"));
+        Optional<Fluid> ceiExperience = Optional.ofNullable(ForgeRegistries.FLUIDS.getValue(
+                ResourceLocation.fromNamespaceAndPath("create_enchantment_industry", "experience")));
         ceiExperience.ifPresent(fluidExperience -> JarUnpackingHandler.REGISTRY.register(
                 fluidExperience, (level, pos, fluid, player) -> {
                     // default is 1mb -> 1xp
                     int experience = fluid.getAmount();
-                    if (player != null) {
-                        player.giveExperiencePoints(experience);
-                    } else {
-                        ExperienceOrb.award(level, pos.getCenter(), experience);
-                    }
+
+                    ExperienceOrb.award(level, pos.getCenter(), experience);
                     return true;
                 }));
 
@@ -33,11 +30,8 @@ public class FactoryJarUnpackingHandlers {
                         (level, pos, fluid, player) -> {
                             // default is 20mb -> 1xp
                             int experience = fluid.getAmount() / 20;
-                            if (player != null) {
-                                player.giveExperiencePoints(experience);
-                            } else {
-                                ExperienceOrb.award(level, pos.getCenter(), experience);
-                            }
+
+                            ExperienceOrb.award(level, pos.getCenter(), experience);
                             return true;
                         }));
     }
