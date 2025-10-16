5c59de2d042644433b7bb6a0fadce39af42b94ca
zznty <94796179+zznty@users.noreply.github.com>
2025-10-17 01:18:15 +0700

    fix: tick restocker promises

--- forge/src/main/java/ru/zznty/create_factory_logistics/mixin/logistics/panel/FactoryPanelRequestMixin.java
+++ forge/src/main/java/ru/zznty/create_factory_logistics/mixin/logistics/panel/FactoryPanelRequestMixin.java
@@ -210,6 +210,8 @@ public abstract class FactoryPanelRequestMixin extends FilteringBehaviour implem
         FactoryPanelBlockEntity panelBE = panelBE();
         if (targetedBy.isEmpty() && !panelBE.restocker)
             return;
+        if (panelBE.restocker)
+            restockerPromises.tick();
         if (satisfied || promisedSatisfied || waitingForNetwork || redstonePowered)
             return;
         if (timer > 0) {
