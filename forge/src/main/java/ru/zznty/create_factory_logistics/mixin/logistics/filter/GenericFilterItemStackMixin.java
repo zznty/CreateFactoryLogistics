3b0704c234f0fe88440e7e6b8736e8bd913a7907
zznty <94796179+zznty@users.noreply.github.com>
2025-10-27 22:15:24 +0700

    chore: add generic variant of filter

---
+++ forge/src/main/java/ru/zznty/create_factory_logistics/mixin/logistics/filter/GenericFilterItemStackMixin.java
@@ -0,0 +1,24 @@
+package ru.zznty.create_factory_logistics.mixin.logistics.filter;
+
+import com.simibubi.create.content.logistics.filter.FilterItemStack;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.fluids.FluidStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import ru.zznty.create_factory_abstractions.generic.support.GenericFilterItemStack;
+import ru.zznty.create_factory_logistics.logistics.generic.FluidGenericStack;
+
+@Mixin(GenericFilterItemStack.class)
+public class GenericFilterItemStackMixin extends FilterItemStack {
+    protected GenericFilterItemStackMixin(ItemStack filter) {
+        super(filter);
+    }
+
+    @Overwrite
+    public boolean test(Level world, FluidStack stack, boolean matchNBT) {
+        if (isEmpty()) return true;
+        //noinspection DataFlowIssue
+        return ((GenericFilterItemStack) (Object) this).test(world, FluidGenericStack.wrap(stack), matchNBT);
+    }
+}
